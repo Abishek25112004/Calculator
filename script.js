c9768ba761f538177b5e4c61d8a4f04bfc44358c
Abishek M <167514119+Abishek25112004@users.noreply.github.com>
2025-06-16 19:21:26 +0530

    completing function for calculations and displaying the result

--- script.js
+++ script.js
@@ -9,24 +9,69 @@ let two = document.querySelector('.two')
 let one = document.querySelector('.one')
 let zero = document.querySelector('.zero')
 let twoZeros = document.querySelector('.twoZeros')
+let dot = document.querySelector('.dot')
 
-let btnArray = [nine,eight,seven,six,five,four,three,two,one,zero,twoZeros]
+let btnArray = [nine,eight,seven,six,five,four,three,two,one,zero,twoZeros,dot]
 
 let currentInput = document.querySelector('.currentInput')
 
-let display=(btnValue)=>{
+let display=()=>{
     currentInput.innerText = value
 }
 
 let value = " ";
 
+let valueArray = []
+
 btnArray.map((btn)=>{
      btn.addEventListener('click',()=>{
-        value = value+btn.value
+        value += btn.value
         console.log(value)
-
-        display(value)
+        display()
      })
 })
+
+let add = document.querySelector('.add')
+let sub = document.querySelector('.sub')
+let mul = document.querySelector('.mul')
+let div = document.querySelector('.div')
+let mod = document.querySelector('.mod')
+
+let calArray = [add,sub,mul,div,mod]
+
+calArray.map((calBtn)=>{
+    calBtn.addEventListener('click',()=>{
+        value+=calBtn.value
+        display()
+    })
+})
+
+let equal = document.querySelector('.equal')
+let displayAns = document.querySelector('.displayAns')
+
+let result 
+
+equal.addEventListener('click',()=>{
+    result = eval(value)
+    console.log(result)
+    displayAns.innerHTML = result
+})
+
+let clear = document.querySelector('.clear')
+
+clear.addEventListener('click',()=>{
+    currentInput.innerText = 0
+    value = ' '
+    displayAns.innerHTML = 0
+    result = 0
+})
+
+
+
+
+
+
+
+
 
 
